75d3b41d1a58150b2cc2e8cb5b2252ee78c6d201
Sapan Bhatia <gwsapan@gmail.com>
2017-03-29 21:10:29 +0200

    Package based installer

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='plyprotobuf',
+      version='1.0',
+      description='Protobuf Parsing Library that uses ply',
+      author='Dusan Klinec',
+      url='https://github.com/sb98052/plyprotobuf',
+      packages=['plyproto'],
+     )
